342bb49bdf969f2e031972bf2f43f0be9d412489
sakoPO <sako_po@poczta.fm>
2016-12-05 02:50:41 +0100

    Fixed typo in lsm330dl driver

--- drivers/STMicroelectronics/LSM330DL/driver/lsm330dlAccelerometer.cpp
+++ drivers/STMicroelectronics/LSM330DL/driver/lsm330dlAccelerometer.cpp
@@ -1,4 +1,4 @@
-/* ========================================================================================================================== *//**
+/* ========================================================================================================================== */ /**
  @license    BSD 3-Clause
  @copyright  microHAL
  @version    $Id$
@@ -24,12 +24,9 @@
  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
- *//* ========================================================================================================================== */
+ */ /* ==========================================================================================================================
+                                                                                                                                         */
 
-#include "lsm330dlaccelerometer.h"
+#include "lsm330dlAccelerometer.h"
 
-namespace lsm330 {
-
-
-
-} // namespace lsm330
+namespace lsm330 {}  // namespace lsm330
