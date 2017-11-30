c23fd24ce8c0c257dee541d8f7864ad241ce1e1c
sakoPO <sako.po@gmail.com>
2017-12-01 00:14:54 +0100

    Addted os tests; Added std::future and std::call_once FreeRTOS implementation tests.

---
+++ tests/os/call_once/src/bsp/stm32f4-discovery/bsp.h
@@ -0,0 +1,27 @@
+/* =========================================================================================== *//**
+ @file        STM32F4Discovery.h
+ @authors     Michal Karwatowski
+ @version     $Id$
+ @package
+ @brief       hardware definitions
+ @hardware    STM32F4Discovery
+ @copyright   $Copyright$
+ @details
+
+ *//* ============================================================================================ */
+/* Created on: 20 sty 2014 */
+
+#ifndef STM32F4DISCOVERY_H_
+#define STM32F4DISCOVERY_H_
+
+static microhal::SerialPort *serialPorts[] = {
+		&microhal::stm32f4xx::SerialPort::Serial1,
+		&microhal::stm32f4xx::SerialPort::Serial2,
+		&microhal::stm32f4xx::SerialPort::Serial3,
+		&microhal::stm32f4xx::SerialPort::Serial4//,
+		//microhal::stm32f4xx::SerialPort::Serial5,
+		//microhal::stm32f4xx::SerialPort::Serial6
+};
+
+
+#endif /* STM32F4DISCOVERY_H_ */
