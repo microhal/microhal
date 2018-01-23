3bf9cdb48657300f085f0fff445d9e7d1f9216f1
sakoPO <sako.po@gmail.com>
2018-01-23 21:04:58 +0100

    tests, added usb to serial port converters identyfication by serial number.

--- tests/scripts/common/flash.py
+++ tests/scripts/common/flash.py
@@ -6,7 +6,7 @@ def program(fullPathToBinary, board):
     boardType = interface.getBoardType(board)
     serial = interface.getInterfaceSerialNumber(board)
 
-    parameters = '-f board/' + boardType + ' -c "hla_serial ' + serial + '" -c init -c targets -c "reset halt" -c "sleep 100" -c "flash write_image erase ' + fullPathToBinary + '" -c "verify_image ' + fullPathToBinary +'" -c "reset run" -c shutdown'	
+    parameters = '-d -f board/' + boardType + ' -c "hla_serial ' + serial + '" -c init -c targets -c "reset halt" -c "sleep 100" -c "flash write_image erase ' + fullPathToBinary + '" -c "verify_image ' + fullPathToBinary +'" -c "reset run" -c shutdown'	
     return os.system("/home/pawel/bin/microide/tools/openocd/0.10.0/bin/openocd " + parameters + " 2>> log.log") == 0
 
 def resetHalt(board):
