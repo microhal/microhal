914d4455b8dbe643cd62bfd548e5d5472e4e75bc
sakoPO <sako.po@gmail.com>
2018-02-13 22:42:31 +0100

    updated drivers.

--- drivers/Freescale Semiconductor/MPL115A2/driver/mpl115a2.h
+++ drivers/Freescale Semiconductor/MPL115A2/driver/mpl115a2.h
@@ -2,7 +2,7 @@
  * @license    BSD 3-Clause
  * @copyright  microHAL
  * @version    $Id$
- * @brief      
+ * @brief
  *
  * @authors    Pawel Okas
  * created on: 12-02-2017
@@ -32,108 +32,121 @@
 /* **************************************************************************************************************************************************
  * INCLUDES
  */
-#include "microhal.h"
-#include "I2CDevice/I2CDevice.h"
-#include "units/pressure.h"
 #include <experimental/optional>
+#include "I2CDevice/I2CDevice.h"
+#include "microhal.h"
+#include "units/pressure.h"
 /* **************************************************************************************************************************************************
  * CLASS
  */
 class MPL115A2 : microhal::I2CDevice {
-	using Endianness = microhal::Endianness;
+    using Endianness = microhal::Endianness;
+    using Access = microhal::Access;
+    using Error = microhal::I2C::Error;
 
-	enum Registers {
-		Padc_MSB = 0x00, ///< 10-bit Pressure ADC output value MSB
-		Padc_LSB, ///< 10-bit Pressure ADC output value LSB
-		Tadc_MSB, ///< 10-bit Temperature ADC output value MSB
-		Tadc_LSB, ///< 10-bit Temperature ADC output value LSB
-		a0_MSB, ///< a0 coefficient MSB
-		a0_LSB, ///< a0 coefficient LSB
-		b1_MSB, ///< b1 coefficient MSB
-		b1_LSB, ///< b1 coefficient LSB
-		b2_MSB, ///< b2 coefficient MSB
-		b2_LSB, ///< b2 coefficient LSB
-		c12_MSB, ///< c12 coefficient MSB
-		c12_LSB, ///< c12 coefficient LSB
-		CONVERT = 0x12 ///< Start Pressure and Temperature Conversion
-	};
+    struct Registers {
+        static constexpr auto Padc = microhal::makeRegister<uint16_t, Access::ReadOnly, Endianness::Big>(
+            microhal::Address<uint8_t, 0x00>{});  ///< 10-bit Pressure ADC output value MSB
+        static constexpr auto Tadc = microhal::makeRegister<uint16_t, Access::ReadOnly, Endianness::Big>(
+            microhal::Address<uint8_t, 0x02>{});  ///< 10-bit Temperature ADC output value MSB
+        static constexpr auto a0 =
+            microhal::makeRegister<int16_t, Access::ReadOnly, Endianness::Big>(microhal::Address<uint8_t, 0x04>{});  ///< a0 coefficient MSB
+        static constexpr auto b1 =
+            microhal::makeRegister<int16_t, Access::ReadOnly, Endianness::Big>(microhal::Address<uint8_t, 0x06>{});  ///< b1 coefficient MSB
+        static constexpr auto b2 =
+            microhal::makeRegister<int16_t, Access::ReadOnly, Endianness::Big>(microhal::Address<uint8_t, 0x08>{});  ///< b2 coefficient MSB
+        static constexpr auto c12 =
+            microhal::makeRegister<int16_t, Access::ReadOnly, Endianness::Big>(microhal::Address<uint8_t, 0x0A>{});  ///< c12 coefficient MSB
+        static constexpr auto CONVERT =
+            microhal::makeRegister<uint8_t, Access::ReadWrite>(microhal::Address<uint8_t, 0xF8>{});  ///< Start Pressure and Temperature Conversion
+    };
+
+    //	enum Registers {
+    //		Padc_MSB = 0x00, ///< 10-bit Pressure ADC output value MSB
+    //		Padc_LSB, ///< 10-bit Pressure ADC output value LSB
+    //		Tadc_MSB, ///< 10-bit Temperature ADC output value MSB
+    //		Tadc_LSB, ///< 10-bit Temperature ADC output value LSB
+    //		a0_MSB, ///< a0 coefficient MSB
+    //		a0_LSB, ///< a0 coefficient LSB
+    //		b1_MSB, ///< b1 coefficient MSB
+    //		b1_LSB, ///< b1 coefficient LSB
+    //		b2_MSB, ///< b2 coefficient MSB
+    //		b2_LSB, ///< b2 coefficient LSB
+    //		c12_MSB, ///< c12 coefficient MSB
+    //		c12_LSB, ///< c12 coefficient LSB
+    //		CONVERT = 0x12 ///< Start Pressure and Temperature Conversion
+    //	};
  public:
-	using Pressure = microhal::Pressure<float>;
+    using Pressure = microhal::Pressure<float>;
 
-	MPL115A2(microhal::I2C &i2c) : I2CDevice(i2c, 0xC0) {}
+    MPL115A2(microhal::I2C &i2c) : I2CDevice(i2c, 0xC0) {}
 
-	bool init() {
-		return readCoefficient();
-	}
+    bool init() { return readCoefficient(); }
 
-	bool startConversion() {
-		return writeRegister(CONVERT, (uint8_t)0x00);
-	}
+    Error startConversion() { return write(Registers::CONVERT, (uint8_t)0x00); }
 
-	std::experimental::optional<Pressure> pressure() {
-		std::experimental::optional<Pressure> pressure;
-		Pressure tmp;
-		if (readData(tmp)) {
-			pressure = tmp;
-		}
+    std::experimental::optional<Pressure> pressure() {
+        std::experimental::optional<Pressure> pressure;
+        Pressure tmp;
+        if (readData(tmp)) {
+            pressure = tmp;
+        }
 
-		return pressure;
-	}
+        return pressure;
+    }
 
-	void debug();
+    void debug();
 
-	static constexpr auto maxConversionTime() noexcept {
-		return std::chrono::milliseconds {3};
-	}
+    static constexpr auto maxConversionTime() noexcept { return std::chrono::milliseconds{3}; }
 
  private:
-	struct Coefficient {
-		float a0;
-		float b1;
-		float b2;
-		float c12;
-	} coefficient;
+    struct Coefficient {
+        float a0;
+        float b1;
+        float b2;
+        float c12;
+    } coefficient;
 
-	bool readCoefficient() {
-		uint16_t tmp[4];
-		if (readRegisters(a0_MSB, tmp, 4, Endianness::Big)) {
-			float a0 = static_cast<int16_t>(tmp[0]);
-			a0 /= 1<<3;
-			coefficient.a0 = a0;
-			float b1 = static_cast<int16_t>(tmp[1]);
-			b1 /= 1<<13;
-			coefficient.b1 = b1;
-			float b2 = static_cast<int16_t>(tmp[2]);
-			b2 /= 1<<14;
-			coefficient.b2 = b2;
-			float c12 = static_cast<int16_t>(tmp[3]);
-			c12 /= 1<<23;
-			coefficient.c12 = c12;
-			return true;
-		}
-		return false;
-	}
+    bool readCoefficient() {
+        std::array<int16_t, 4> tmp;
+        if (readRegisters(tmp, Registers::a0, Registers::b1, Registers::b2, Registers::c12) == Error::None) {
+            float a0 = tmp[0];
+            a0 /= 1 << 3;
+            coefficient.a0 = a0;
+            float b1 = tmp[1];
+            b1 /= 1 << 13;
+            coefficient.b1 = b1;
+            float b2 = tmp[2];
+            b2 /= 1 << 14;
+            coefficient.b2 = b2;
+            float c12 = tmp[3];
+            c12 /= 1 << 23;
+            coefficient.c12 = c12;
+            return true;
+        }
+        return false;
+    }
 
-	Pressure compensate(uint16_t rawPressure, uint16_t rawTemperature) const noexcept {
-		rawPressure >>= 6;
-		rawTemperature >>= 6;
-		float Pcomp = coefficient.a0 + (coefficient.b1 + coefficient.c12 * rawTemperature) * rawPressure + coefficient.b2 * rawTemperature;
-		return Pressure {Pcomp * ((115.0f-50.0f)/1023.0f) + 50.0f};
-	}
+    Pressure compensate(uint16_t rawPressure, uint16_t rawTemperature) const noexcept {
+        rawPressure >>= 6;
+        rawTemperature >>= 6;
+        float Pcomp = coefficient.a0 + (coefficient.b1 + coefficient.c12 * rawTemperature) * rawPressure + coefficient.b2 * rawTemperature;
+        return Pressure{Pcomp * ((115.0f - 50.0f) / 1023.0f) + 50.0f};
+    }
 
-	bool readData(Pressure & compensatedPressure) {
-		uint16_t tmp[2];
-		if (readRegisters(Padc_MSB, tmp, 2, Endianness::Big)) {
-//			uint16_t pressure = tmp[0] >> 6;
-//			uint16_t temp = tmp[1] >> 6;
-//
-//			float Pcomp = coefficient.a0 + (coefficient.b1 + coefficient.c12 * temp) * pressure + coefficient.b2 * temp;
-//			Pressure tmp(Pcomp * ((115.0-50.0)/1023.0) + 50.0);
-			compensatedPressure = compensate(tmp[0], tmp[1]);
-			return true;
-		}
-		return false;
-	}
+    bool readData(Pressure &compensatedPressure) {
+        std::array<uint16_t, 2> tmp;
+        if (readRegisters(tmp, Registers::Padc, Registers::Tadc) == Error::None) {
+            //			uint16_t pressure = tmp[0] >> 6;
+            //			uint16_t temp = tmp[1] >> 6;
+            //
+            //			float Pcomp = coefficient.a0 + (coefficient.b1 + coefficient.c12 * temp) * pressure + coefficient.b2 * temp;
+            //			Pressure tmp(Pcomp * ((115.0-50.0)/1023.0) + 50.0);
+            compensatedPressure = compensate(tmp[0], tmp[1]);
+            return true;
+        }
+        return false;
+    }
 };
 
 #endif  // _MICROHAL_MPL115A2_H_
