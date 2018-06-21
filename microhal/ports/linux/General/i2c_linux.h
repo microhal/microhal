60f60fde04f3d5595de115dbc88ced55b734a35e
Pawel <sako_po@poczta.fm>
2018-06-21 17:42:04 +0200

    removed unneded include

--- microhal/ports/linux/General/i2c_linux.h
+++ microhal/ports/linux/General/i2c_linux.h
@@ -30,15 +30,14 @@
 #ifndef _MICROHAL_I2C_LINUX_H_
 #define _MICROHAL_I2C_LINUX_H_
 
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include <algorithm>
 #include <cstdint>
-#include "interfaces/I2cDev.h"
-#include <sys/ioctl.h>
-#include <linux/i2c-dev.h>
 #include <cstdio>
 #include <cstdlib>
-#include <fcntl.h>
-#include <unistd.h>
 #include <iostream>
 
 namespace microhal {
@@ -46,63 +45,55 @@ namespace linux {
 
 class I2C : public microhal::I2C {
  public:
-	I2C(const char *filename) {
-		file = ::open(filename, O_RDWR);
+    I2C(const char *filename) {
+        file = ::open(filename, O_RDWR);
 
-		if (file < 0){
-			std::cout << "unable to open I2C interface.";
-			std::abort();
-		}
-	}
-	~I2C() {
-		close(file);
-	}
+        if (file < 0) {
+            std::cout << "unable to open I2C interface.";
+            std::abort();
+        }
+    }
+    ~I2C() { close(file); }
 
-    bool speed(Speed speed, Mode mode) noexcept final {
-    	std::terminate();
-    }
-    Speed speed() noexcept final {
-    	std::abort();
-    }
-    void busReset() noexcept final {
-    	std::abort();
-    }
+    bool speed(Speed speed, Mode mode) noexcept final { std::terminate(); }
+    Speed speed() noexcept final { std::abort(); }
+    void busReset() noexcept final { std::abort(); }
 
     Error write(DeviceAddress deviceAddress, const uint8_t *data, size_t length) noexcept final {
-    	if (::ioctl(file, I2C_SLAVE, deviceAddress >> 1) < 0) return Error::Unknown;
-    	if (::write(file, data, length) != length) return Error::Unknown;
-    	return Error::None;
+        if (::ioctl(file, I2C_SLAVE, deviceAddress >> 1) < 0) return Error::Unknown;
+        if (::write(file, data, length) != length) return Error::Unknown;
+        return Error::None;
     }
-    Error write(DeviceAddress deviceAddress, const uint8_t* data, size_t dataLength, const uint8_t* dataB, size_t dataBLength) noexcept final {
-    	if (::ioctl(file, I2C_SLAVE, deviceAddress >> 1) < 0) return Error::Unknown;
-    	uint8_t *buffer = new uint8_t[dataLength + dataBLength];
-    	std::copy_n(data, dataLength, buffer);
-    	std::copy_n(dataB, dataBLength, buffer + dataLength);
-    	if (::write(file, buffer, dataLength + dataBLength) != dataLength + dataBLength) return Error::Unknown;
-    	return Error::None;
+    Error write(DeviceAddress deviceAddress, const uint8_t *data, size_t dataLength, const uint8_t *dataB, size_t dataBLength) noexcept final {
+        if (::ioctl(file, I2C_SLAVE, deviceAddress >> 1) < 0) return Error::Unknown;
+        uint8_t *buffer = new uint8_t[dataLength + dataBLength];
+        std::copy_n(data, dataLength, buffer);
+        std::copy_n(dataB, dataBLength, buffer + dataLength);
+        if (::write(file, buffer, dataLength + dataBLength) != dataLength + dataBLength) return Error::Unknown;
+        return Error::None;
     }
     Error read(DeviceAddress deviceAddress, uint8_t *data, size_t size) noexcept final {
-    	if (::ioctl(file, I2C_SLAVE, deviceAddress >> 1) < 0) return Error::Unknown;
-    	if (::read(file, data, size) != size) return Error::Unknown;
-    	return Error::None;
+        if (::ioctl(file, I2C_SLAVE, deviceAddress >> 1) < 0) return Error::Unknown;
+        if (::read(file, data, size) != size) return Error::Unknown;
+        return Error::None;
     }
     Error read(DeviceAddress deviceAddress, uint8_t *data, size_t dataLength, uint8_t *dataB, size_t dataBLength) noexcept final {
-    	if (::ioctl(file, I2C_SLAVE, deviceAddress >> 1) < 0) return Error::Unknown;
-    	uint8_t *buffer = new uint8_t[dataLength + dataBLength];
-    	if (::read(file, buffer, dataLength + dataBLength) != dataLength  + dataBLength) return Error::Unknown;
-    	std::copy_n(buffer, dataLength, data);
-    	std::copy_n(buffer + dataLength, dataBLength, dataB);
-    	return Error::None;
+        if (::ioctl(file, I2C_SLAVE, deviceAddress >> 1) < 0) return Error::Unknown;
+        uint8_t *buffer = new uint8_t[dataLength + dataBLength];
+        if (::read(file, buffer, dataLength + dataBLength) != dataLength + dataBLength) return Error::Unknown;
+        std::copy_n(buffer, dataLength, data);
+        std::copy_n(buffer + dataLength, dataBLength, dataB);
+        return Error::None;
     }
-    Error writeRead(DeviceAddress deviceAddress, const uint8_t* write, size_t writeLength, uint8_t *read, size_t readLength) noexcept final {
-    	if (::ioctl(file, I2C_SLAVE, deviceAddress >> 1) < 0) return Error::Unknown;
-    	if (::write(file, write, writeLength) != writeLength) return Error::Unknown;
-    	if (::read(file, read, readLength) != readLength) return Error::Unknown;
-    	return Error::None;
+    Error writeRead(DeviceAddress deviceAddress, const uint8_t *write, size_t writeLength, uint8_t *read, size_t readLength) noexcept final {
+        if (::ioctl(file, I2C_SLAVE, deviceAddress >> 1) < 0) return Error::Unknown;
+        if (::write(file, write, writeLength) != writeLength) return Error::Unknown;
+        if (::read(file, read, readLength) != readLength) return Error::Unknown;
+        return Error::None;
     }
+
  private:
     int file;
-
 };
 
 }  // namespace linux
