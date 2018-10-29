a61960c3ae61a1ec7924692f5fd0fd05b0855836
pokas <sakoPO@github.com>
2018-10-29 21:18:53 +0100

    Updated MCP23017 driver and example to work with new GPIO API and new I2C API.

--- drivers/Microchip/MCP23017/driver/mcp23017.h
+++ drivers/Microchip/MCP23017/driver/mcp23017.h
@@ -43,8 +43,38 @@ class MCP23017 {
     using LogLevel = microhal::diagnostic::LogLevel;
     using Error = microhal::I2C::Error;
     using Pin = uint8_t;
-    using Endianness = microhal::Endianness;
 
+ private:
+    using Access = microhal::Access;
+    using Endianness = microhal::Endianness;
+    // create alias to microhal::Address, we just want to type less
+    template <typename T, T i>
+    using Address = microhal::Address<T, i>;
+
+    static constexpr auto IODIR =
+        microhal::makeRegister<uint16_t, Access::ReadWrite, Endianness::Little>(Address<uint8_t, 0x00>{});  ///< I/O DIRECTION REGISTER (ADDR 0x00)
+    static constexpr auto IPOL = microhal::makeRegister<uint16_t, Access::ReadOnly, Endianness::Little>(
+        Address<uint8_t, 0x02>{});  ///< INPUT POLARITY PORT REGISTER (ADDR 0x01)
+    static constexpr auto GPINTEN =
+        microhal::makeRegister<uint16_t, Access::ReadOnly, Endianness::Little>(Address<uint8_t, 0x04>{});  ///< INTERRUPT-ON-CHANGE PINS (ADDR 0x02)
+    static constexpr auto DEFVAL =
+        microhal::makeRegister<uint16_t, Access::ReadOnly, Endianness::Little>(Address<uint8_t, 0x06>{});  ///< DEFAULT VALUE REGISTER (ADDR 0x03)
+    static constexpr auto INTCON = microhal::makeRegister<uint16_t, Access::ReadOnly, Endianness::Little>(
+        Address<uint8_t, 0x08>{});  ///< INTERRUPT-ON-CHANGE CONTROL REGISTER (ADDR 0x04)
+    static constexpr auto IOCON = microhal::makeRegister<uint16_t, Access::ReadOnly, Endianness::Little>(
+        Address<uint8_t, 0x0A>{});  ///< I/O EXPANDER CONFIGURATION REGISTER (ADDR 0x05)
+    static constexpr auto GPPU = microhal::makeRegister<uint16_t, Access::ReadWrite, Endianness::Little>(
+        Address<uint8_t, 0x0C>{});  ///< GPIO PULL-UP RESISTOR REGISTER (ADDR 0x06)
+    static constexpr auto INTF =
+        microhal::makeRegister<uint16_t, Access::ReadOnly, Endianness::Little>(Address<uint8_t, 0x0E>{});  ///< INTERRUPT FLAG REGISTER (ADDR 0x07)
+    static constexpr auto INTCAP = microhal::makeRegister<uint16_t, Access::ReadOnly, Endianness::Little>(
+        Address<uint8_t, 0x10>{});  ///< INTERRUPT CAPTURED VALUE FOR PORT REGISTER (ADDR 0x08)
+    static constexpr auto GPIO = microhal::makeRegister<uint16_t, Access::ReadWrite, Endianness::Little>(
+        Address<uint8_t, 0x12>{});  ///< GENERAL PURPOSE I / O PORT REGISTER(ADDR 0x09)
+    static constexpr auto OLAT =
+        microhal::makeRegister<uint16_t, Access::ReadOnly, Endianness::Little>(Address<uint8_t, 0x14>{});  ///< OUTPUT LATCH REGISTER 0 (ADDR 0x0A)
+
+ public:
     enum Address : uint8_t {
         Addr_0x40 = 0b01000000,
         Addr_0x42 = 0b01000010,
@@ -67,37 +97,17 @@ class MCP23017 {
     };
 
     MCP23017(microhal::I2C &i2c, uint8_t address) : i2c(i2c, address) {
-        if (address > Address::Addr_0x4E || address < Address::Addr_0x40) {
+        if (address > Addr_0x4E || address < Addr_0x40) {
             log << microhal::diagnostic::lock << MICROHAL_EMERGENCY << "I2C address out of range." << microhal::diagnostic::unlock;
             std::terminate();  // critical error, provided I2C address out of range
         }
     }
     virtual ~MCP23017();
 
-    Error setPort(uint16_t values) {
-        if (i2c.setBitsInRegister(GPIO, values, Endianness::Little))
-            return Error::None;
-        else
-            return i2c.getLastError();
-    }
-    Error resetPort(uint16_t values) {
-        if (i2c.clearBitsInRegister(GPIO, values, Endianness::Little))
-            return Error::None;
-        else
-            return i2c.getLastError();
-    }
-    Error getPort(uint16_t &values) {
-        if (i2c.readRegister(GPIO, values, Endianness::Little))
-            return Error::None;
-        else
-            return i2c.getLastError();
-    }
-    Error getPortDirection(uint16_t &dir) {
-        if (i2c.readRegister(IODIR, dir, Endianness::Little))
-            return Error::None;
-        else
-            return i2c.getLastError();
-    }
+    Error setPort(uint16_t values) { return i2c.setBitsInRegister(GPIO, values); }
+    Error resetPort(uint16_t values) { return i2c.clearBitsInRegister(GPIO, values); }
+    Error getPort(uint16_t &values) { return i2c.readRegister(GPIO, values); }
+    Error getPortDirection(uint16_t &dir) { return i2c.readRegister(IODIR, dir); }
 
     Error setPin(Pin pin) { return setPort(1 << pin); }
     Error resetPin(Pin pin) { return resetPort(1 << pin); }
@@ -109,7 +119,7 @@ class MCP23017 {
     }
     /** @brief Function for pin initialization, this function allow to configure only one pin if you want to configure multiple pins at single
      * function call
-     * please use @ref congifurePort metod.
+     * please use @ref congifurePort method.
      *
      * @param pinNumber from 0 to 15
      * @param direction desired pin direction @ref Direction
@@ -119,53 +129,43 @@ class MCP23017 {
     Error configurePin(Pin pinNumber, Direction direction, PullConfiguration pull) {
         if (pinNumber > pinCount - 1) return Error::Unknown;
 
-        if (i2c.writeRegisterWithMask(IODIR, uint16_t{static_cast<uint16_t>(direction) << pinNumber}, 1 << pinNumber, Endianness::Little)) {
-            if (i2c.writeRegisterWithMask(GPPU, uint16_t{static_cast<uint16_t>(pull) << pinNumber}, 1 << pinNumber, Endianness::Little)) {
-                return Error::None;
-            }
+        auto status = i2c.modifyBitsInRegister(IODIR, uint16_t{static_cast<uint16_t>(direction) << pinNumber}, 1 << pinNumber);
+        if (status == Error::None) {
+            status = i2c.modifyBitsInRegister(GPPU, uint16_t{static_cast<uint16_t>(pull) << pinNumber}, 1 << pinNumber);
         }
 
-        return i2c.getLastError();
+        return status;
     }
     /** @brief This function allow you to configure all pins of MCP23017 in single function call.
      *
      * Notice: All previous configuration will be overwritten by this function.
      *
-     * @param direction - 16bit data representing pin direction, when bit is set to 1 then corresponding pin will be configured as output, when bit is
-     * equal to 0 then corresponding pin will be configured as input; LSB is corresponding to PORTA.0, MSB is corresponding to PORTB.7. So if you want
-     * to configure PORTA.1 and PORTB.0 as output you have to pass 0b0000'0001'0000'0010 as direction value. All other pins will be configured as
-     * input.
+     * @param direction - 16bit data representing pin direction, when bit is set to 1 then corresponding pin will be configured as output, when
+     * bit is equal to 0 then corresponding pin will be configured as input; LSB is corresponding to PORTA.0, MSB is corresponding to PORTB.7. So
+     * if you want to configure PORTA.1 and PORTB.0 as output you have to pass 0b0000'0001'0000'0010 as direction value. All other pins will be
+     * configured as input.
      * @param pullUp - 16bit data representing pin pull-up state. If bit is set to 1 then corresponding pin will have enabled pull-up. Bits are
      * organized same as in direction parameter. Example: if you want to enable pull-up on PORTA.0 and PORTB.1 you have pass 0b0000'0010'0000'0001
      * @return
      */
     Error configurePort(uint16_t direction, uint16_t pullUp) {
-        if (i2c.writeRegister(IODIR, direction, Endianness::Little)) {
-            if (i2c.writeRegister(GPPU, pullUp, Endianness::Little)) {
-                return Error::None;
-            }
+        auto status = i2c.writeRegister(IODIR, direction);
+        if (status == Error::None) {
+            status = i2c.writeRegister(GPPU, pullUp);
         }
-        return i2c.getLastError();
+
+        return status;
     }
 
-    Error getIOCon(uint8_t &val) { i2c.readRegister(IOCON, val); }
+    Error getIOCon(uint16_t &val) { return i2c.readRegister(IOCON, val); }
 
-    Error init() { i2c.writeRegister(0x05, uint8_t{0b00000000}); }
+    Error init() {
+        static constexpr auto Config =
+            microhal::makeRegister<uint8_t, Access::ReadWrite>(Address<uint8_t, 0x05>{});  ///< I/O DIRECTION REGISTER (ADDR 0x00)
+        return i2c.writeRegister(Config, uint8_t{0b00000000});
+    }
 
  private:
-    enum Register : uint8_t {
-        IODIR = 0x00,    ///< I/O DIRECTION REGISTER (ADDR 0x00)
-        IPOL = 0x02,     ///< INPUT POLARITY PORT REGISTER (ADDR 0x01)
-        GPINTEN = 0x04,  ///< INTERRUPT-ON-CHANGE PINS (ADDR 0x02)
-        DEFVAL = 0x06,   ///< DEFAULT VALUE REGISTER (ADDR 0x03)
-        INTCON = 0x08,   ///< INTERRUPT-ON-CHANGE CONTROL REGISTER (ADDR 0x04)
-        IOCON = 0x0A,    ///< I/O EXPANDER CONFIGURATION REGISTER (ADDR 0x05)
-        GPPU = 0x0C,     ///< GPIO PULL-UP RESISTOR REGISTER (ADDR 0x06)
-        INTF = 0x0E,     ///< INTERRUPT FLAG REGISTER (ADDR 0x07)
-        INTCAP = 0x10,   ///< INTERRUPT CAPTURED VALUE FOR PORT REGISTER (ADDR 0x08)
-        GPIO = 0x12,     ///< GENERAL PURPOSE I / O PORT REGISTER(ADDR 0x09)
-        OLAT = 0x14,     ///< OUTPUT LATCH REGISTER 0 (ADDR 0x0A)
-    };
     microhal::I2CDevice i2c;
     microhal::diagnostic::Diagnostic<LogLevel::Disabled> log;
     static constexpr unsigned int pinCount = 16;
