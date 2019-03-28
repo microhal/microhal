7c395f9aafe3ddcfaa31ed4e3796578ea36cc756
pokas <sakoPO@github.com>
2019-03-28 22:07:11 +0100

    Added core, externalInterrupt and syscals for lpc43xx

---
+++ microhal/ports/lpc43xx/core_lpc43xx.h
@@ -0,0 +1,19 @@
+#ifndef _MICROHAL_CORE_LPC43XX_H_
+#define _MICROHAL_CORE_LPC43XX_H_
+/* **************************************************************************************************************************************************
+ * INCLUDES
+ */
+namespace microhal {
+namespace lpc43xx {
+
+/* **************************************************************************************************************************************************
+ * CLASS
+ */
+class Core {  // this class is not tested completely
+    Core(void) = delete;
+};
+
+}  // namespace lpc43xx
+}  // namespace microhal
+
+#endif  // _MICROHAL_CORE_LPC43XX_H_
