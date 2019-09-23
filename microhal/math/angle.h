bfa6e64d1467bf9cf38d9578935656c073db9763
pokas <sakoPO@github.com>
2019-09-23 22:54:47 +0200

    Updated math/angle.h

--- microhal/math/angle.h
+++ microhal/math/angle.h
@@ -8,35 +8,33 @@
 #ifndef ANGLE_H_
 #define ANGLE_H_
 
-#include <stdint.h>
+#include <cstdint>
+#include "math/constants.h"
+
+namespace microhal {
+namespace math {
 /**
  * \brief This class provides basic angle conversion
  */
 class Angle {
-private:
-    constexpr static float pi =
-            3.1415926535897932384626433832795028841971693993751058209749445923078164062;
-    Angle(){}
-public:
+ private:
+    Angle() {}
+
+ public:
     //---------------------------------- functions ----------------------------------
     /**
      * Radians to degrees conversion
      * @param rad Angle in radians
      * @return Angle in degrees
      */
-    static float radToDeg(float rad)
-    {
-        return rad * 180.0 / pi;
-    }
+    static constexpr float radToDeg(float rad) { return rad * 180.0f / pi; }
     /**
      * Degrees to radians conversion
      * @param deg Angle in degrees
      * @return Angle in radians
      */
-    static float degToRad(float deg)
-    {
-        return deg * pi / 180.0;
-    }
+    static constexpr float degToRad(float deg) { return deg * pi / 180.0f; }
 };
-
+}  // namespace math
+}  // namespace microhal
 #endif /* ANGLE_H_ */
