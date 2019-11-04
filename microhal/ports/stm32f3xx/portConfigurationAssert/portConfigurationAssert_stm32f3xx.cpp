ecc258e86937e0d206406c6e00566519f001f5af
pokas <sakoPO@github.com>
2019-11-04 04:49:05 +0100

    Port STM32F3xx, updated port configuration assertion.

--- microhal/ports/stm32f3xx/portConfigurationAssert/portConfigurationAssert_stm32f3xx.cpp
+++ microhal/ports/stm32f3xx/portConfigurationAssert/portConfigurationAssert_stm32f3xx.cpp
@@ -25,7 +25,7 @@
 #ifndef MICROHAL_SERIAL_PORT1_DMA_RX_STREAM
 #error MICROHAL_SERIAL_PORT1_DMA_RX_STREAM have to be defined inside microhalPortConfig_stm32f3xx.h
 #elif MICROHAL_SERIAL_PORT1_DMA_RX_STREAM != 5  // Serial port 1 RX stream can be connected only to DMA1 Channel 5.
-#error MICROHAL_SERIAL_PORT1_DMA_TX_STREAM have to be equal 5, please check your microhalPortConfig_stm32f3xx.h
+#error MICROHAL_SERIAL_PORT1_DMA_RX_STREAM have to be equal 5, please check your microhalPortConfig_stm32f3xx.h
 #endif
 #endif
 // ********************* Serial Port 2
@@ -40,12 +40,12 @@
 #ifndef MICROHAL_SERIAL_PORT2_DMA_TX_STREAM
 #error MICROHAL_SERIAL_PORT2_DMA_TX_STREAM have to be defined inside microhalPortConfig_stm32f3xx.h
 #elif MICROHAL_SERIAL_PORT2_DMA_TX_STREAM != 7  // Serial port 2 TX stream can be connected only to DMA1 Channel 7.
-#error MICROHAL_SERIAL_PORT2_DMA_TX_STREAM have to be equal 4, please check your microhalPortConfig_stm32f3xx.h
+#error MICROHAL_SERIAL_PORT2_DMA_TX_STREAM have to be equal 7, please check your microhalPortConfig_stm32f3xx.h
 #endif
 #ifndef MICROHAL_SERIAL_PORT2_DMA_RX_STREAM
 #error MICROHAL_SERIAL_PORT2_DMA_RX_STREAM have to be defined inside microhalPortConfig_stm32f3xx.h
 #elif MICROHAL_SERIAL_PORT2_DMA_RX_STREAM != 6  // Serial port 2 RX stream can be connected only to DMA1 Channel 6.
-#error MICROHAL_SERIAL_PORT2_DMA_TX_STREAM have to be equal 5, please check your microhalPortConfig_stm32f3xx.h
+#error MICROHAL_SERIAL_PORT2_DMA_RX_STREAM have to be equal 6, please check your microhalPortConfig_stm32f3xx.h
 #endif
 #endif
 // ********************* Serial Port 3
@@ -60,12 +60,12 @@
 #ifndef MICROHAL_SERIAL_PORT3_DMA_TX_STREAM
 #error MICROHAL_SERIAL_PORT3_DMA_TX_STREAM have to be defined inside microhalPortConfig_stm32f3xx.h
 #elif MICROHAL_SERIAL_PORT3_DMA_TX_STREAM != 2  // Serial port 3 TX stream can be connected only to DMA1 Channel 2.
-#error MICROHAL_SERIAL_PORT3_DMA_TX_STREAM have to be equal 4, please check your microhalPortConfig_stm32f3xx.h
+#error MICROHAL_SERIAL_PORT3_DMA_TX_STREAM have to be equal 2, please check your microhalPortConfig_stm32f3xx.h
 #endif
 #ifndef MICROHAL_SERIAL_PORT3_DMA_RX_STREAM
 #error MICROHAL_SERIAL_PORT3_DMA_RX_STREAM have to be defined inside microhalPortConfig_stm32f3xx.h
 #elif MICROHAL_SERIAL_PORT3_DMA_RX_STREAM != 3  // Serial port 3 RX stream can be connected only to DMA1 Channel 3.
-#error MICROHAL_SERIAL_PORT3_DMA_TX_STREAM have to be equal 5, please check your microhalPortConfig_stm32f3xx.h
+#error MICROHAL_SERIAL_PORT3_DMA_RX_STREAM have to be equal 3, please check your microhalPortConfig_stm32f3xx.h
 #endif
 #endif
 
