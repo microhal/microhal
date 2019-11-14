d276c19cb9672591ccc082bb96a9d55ba46af984
pokas <sakoPO@github.com>
2019-11-14 20:14:41 +0100

    Ports, updated port manager.

--- microhal/ports/manager/STMicroelectronics/core_stm.h
+++ microhal/ports/manager/STMicroelectronics/core_stm.h
@@ -13,6 +13,11 @@
 namespace microhal {
 namespace activePort = stm32f0xx;
 }
+#elif defined(MCU_TYPE_STM32F1XX)
+//#include "ports/stm32f1xx/core_stm32f1xx.h"
+// namespace microhal {
+// namespace activePort = stm32f1xx;
+//}
 #elif defined(MCU_TYPE_STM32F3XX)
 #include "ports/stm32f3xx/core_stm32f3xx.h"
 namespace microhal {
--- microhal/ports/manager/STMicroelectronics/i2c_stm.h
+++ microhal/ports/manager/STMicroelectronics/i2c_stm.h
@@ -13,6 +13,11 @@
 namespace microhal {
 namespace activePort = stm32f0xx;
 }
+#elif defined(MCU_TYPE_STM32F1XX)
+//#include "ports/stm32f1xx/i2c_stm32f1xx.h"
+// namespace microhal {
+// namespace activePort = stm32f1xx;
+//}
 #elif defined(MCU_TYPE_STM32F3XX)
 #include "ports/stm32f3xx/i2c_stm32f3xx.h"
 namespace microhal {
@@ -20,9 +25,9 @@ namespace activePort = stm32f3xx;
 }
 #elif defined(MCU_TYPE_STM32F4XX)
 #include "ports/stm32f4xx/i2c_stm32f4xx.h"
-namespace microhal {
-namespace activePort = stm32f4xx;
-}
+// namespace microhal {
+// namespace activePort = stm32f4xx;
+//}
 #else
 #error "MCU type must be specified."
 #endif
