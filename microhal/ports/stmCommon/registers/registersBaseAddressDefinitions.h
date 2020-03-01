2da8cd7659d9ff36acffdff3a27fd0747bc95d93
Your Name <you@example.com>
2020-03-01 01:00:19 +0100

    Port STM common, updated preprocesor directives in registerBaseAddressDefinitions.h to emit compilation error whem MCU is unspecyfied.

--- microhal/ports/stmCommon/registers/registersBaseAddressDefinitions.h
+++ microhal/ports/stmCommon/registers/registersBaseAddressDefinitions.h
@@ -11,25 +11,19 @@
 #if defined(STM32F051x8)
 #define STM32F0x1
 #include "peripheralBaseAddressDefinitions/registersBaseAddressDefinitions_stm32f0x1.h"
-#endif
-
-#if defined(STM32F103)
+#elif defined(STM32F103)
 #include "peripheralBaseAddressDefinitions/registersBaseAddressDefinitions_stm32f103.h"
-#endif
-
-#if defined(STM32F334x8)
+#elif defined(STM32F334x8)
 #define STM32F3x4
 #include "peripheralBaseAddressDefinitions/registersBaseAddressDefinitions_stm32f3x4.h"
-#endif
-
-#if defined(STM32F407xx)
+#elif defined(STM32F407xx)
 #define STM32F407
 #include "peripheralBaseAddressDefinitions/registersBaseAddressDefinitions_stm32f407.h"
-#endif
-
-#if defined(STM32F411xE)
+#elif defined(STM32F411xE)
 #define STM32F411
 #include "peripheralBaseAddressDefinitions/registersBaseAddressDefinitions_stm32f411.h"
+#else
+#error "MCU type have to be specified."
 #endif
 
 #endif /* SRC_MICROHAL_PORTS_STMCOMMON_REGISTERS_REGISTERSBASEADDRESSDEFINITIONS_H_ */
