9b4c72b72a104266f149471fb5430ffadad5bdc3
Your Name <you@example.com>
2020-04-15 20:58:24 +0200

    Port STM32F1XX, added clockManager.h

---
+++ microhal/ports/stm32f1xx/clockManager.h
@@ -0,0 +1,17 @@
+/*
+ * clockManager.h
+ *
+ *  Created on: Mar 3, 2020
+ *      Author: pokas
+ */
+
+#ifndef SRC_MICROHAL_PORTS_STM32F1XX_CLOCKMANAGER_H_
+#define SRC_MICROHAL_PORTS_STM32F1XX_CLOCKMANAGER_H_
+
+#include "../stmCommon/clockManager/hse.h"
+#include "../stmCommon/clockManager/hsi.h"
+#include "../stmCommon/clockManager/pll.h"
+#include "../stmCommon/clockManager/sysclk.h"
+#include "../stmCommon/clockManager/usbClock.h"
+
+#endif /* SRC_MICROHAL_PORTS_STM32F1XX_CLOCKMANAGER_H_ */
