3832577aa6333af72083a568f0760daa5418cd71
Your Name <you@example.com>
2020-04-17 01:21:11 +0200

    Port STM Common, added driver configuration selector.

---
+++ microhal/ports/stmCommon/driverConfiguration_stmCommon.h
@@ -0,0 +1,15 @@
+/*
+ * driver_version_stmCommon.h
+ *
+ *  Created on: Apr 15, 2020
+ *      Author: pokas
+ */
+
+#ifndef SRC_MICROHAL_PORTS_STMCOMMON_DRIVERCONFIGURATION_STMCOMMON_H_
+#define SRC_MICROHAL_PORTS_STMCOMMON_DRIVERCONFIGURATION_STMCOMMON_H_
+
+#ifdef MCU_TYPE_STM32F1XX
+#include <ports/stm32f1xx/driver_configuration_stm32f1xx.h>
+#endif
+
+#endif /* SRC_MICROHAL_PORTS_STMCOMMON_DRIVERCONFIGURATION_STMCOMMON_H_ */
