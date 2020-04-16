93b461df140d32fdb106e067f74d3c2915773018
Your Name <you@example.com>
2020-04-17 01:19:24 +0200

    Port STM32F1xx, added i2c, pwr, rtc, watchdog and independent watchdog drivers.

---
+++ microhal/ports/stm32f1xx/rtc_stm32f1xx.h
@@ -0,0 +1,9 @@
+#ifndef _MICROHAL_PORTS_STM32F1XX_RTC_STM32F1XX_H_
+#define _MICROHAL_PORTS_STM32F1XX_RTC_STM32F1XX_H_
+
+/* **************************************************************************************************************************************************
+ * INCLUDES
+ */
+#include <ports/stmCommon/rtc_v1/rtc.h>
+
+#endif /* _MICROHAL_PORTS_STM32F1XX_RTC_STM32F1XX_H_ */
