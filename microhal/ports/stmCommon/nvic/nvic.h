5c29680f582f0641810da88603d5bef7df689079
pokas <sakoPO@github.com>
2020-05-05 11:54:36 +0200

    Port STMCommon, added nvic driver placeholder.

---
+++ microhal/ports/stmCommon/nvic/nvic.h
@@ -0,0 +1,13 @@
+/*
+ * nvic.h
+ *
+ *  Created on: Apr 14, 2020
+ *      Author: pokas
+ */
+
+#ifndef _MICROHAL_PORTS_STMCOMMON_NVIC_NVIC_H_
+#define _MICROHAL_PORTS_STMCOMMON_NVIC_NVIC_H_
+
+namespace NVIC {}
+
+#endif /* _MICROHAL_PORTS_STMCOMMON_NVIC_NVIC_H_ */
