7ad0f349df8585d95ce3084039e8619e2f4d061e
sakoPO <sako.po@gmail.com>
2020-12-11 02:07:39 +0100

    Port STMCommon, updated driver configuration.

--- microhal/ports/stmCommon/driverConfiguration_stmCommon.h
+++ microhal/ports/stmCommon/driverConfiguration_stmCommon.h
@@ -8,12 +8,20 @@
 #ifndef SRC_MICROHAL_PORTS_STMCOMMON_DRIVERCONFIGURATION_STMCOMMON_H_
 #define SRC_MICROHAL_PORTS_STMCOMMON_DRIVERCONFIGURATION_STMCOMMON_H_
 
+#ifdef MCU_TYPE_STM32F0XX
+#include <ports/stm32f0xx/driver_configuration_stm32f0xx.h>
+#endif
+
 #ifdef MCU_TYPE_STM32F1XX
 #include <ports/stm32f1xx/driver_configuration_stm32f1xx.h>
 #endif
 
 #ifdef MCU_TYPE_STM32F3XX
 #include <ports/stm32f3xx/driver_configuration_stm32f3xx.h>
+#endif
+
+#ifdef MCU_TYPE_STM32F4XX
+#include <ports/stm32f4xx/driver_configuration_stm32f4xx.h>
 #endif
 
 #endif /* SRC_MICROHAL_PORTS_STMCOMMON_DRIVERCONFIGURATION_STMCOMMON_H_ */
