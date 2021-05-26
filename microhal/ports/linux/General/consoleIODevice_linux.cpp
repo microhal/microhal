2ad501635fc9ef4b7c9013ff1ad87df4350b02fd
sakoPO <sako.po@gmail.com>
2021-05-26 12:31:25 +0200

    Port Linux, consoleIODevice now uses ncurses.

--- microhal/ports/linux/General/consoleIODevice_linux.cpp
+++ microhal/ports/linux/General/consoleIODevice_linux.cpp
@@ -6,118 +6,110 @@
  */
 
 #include "consoleIODevice_linux.h"
+#include <fcntl.h>
+#include <ncurses.h>
 #include <stdio.h>
-//#include <conio.h>
-//#include <ios>
-//#include <cstdio>
-//#include <io.h>
-//#include <fcntl.h>
+#include <cstdio>
+#include <ios>
+
 namespace microhal {
 namespace linux {
 
 console_IODevice consoleIODev;
 
-console_IODevice::console_IODevice() /*: runReadingThread(false), readingThread(),*/
-		/*inputConsole(nullptr), outputConsole(nullptr)*/ {
-
+console_IODevice::console_IODevice() {
+    initscr(); /* Start curses mode 		*/
 }
 
 console_IODevice::~console_IODevice() {
-	stopReadingThread();
+    stopReadingThread();
+    endwin(); /* End curses mode		  */
 }
 
 bool console_IODevice::open(OpenMode mode) noexcept {
+    if (isOpen()) {  // should be first properly close
+        return false;
+    }
 
-	if (isOpen()) { // should be first properly close
-		return false;
-	}
-//	AllocConsole();
-//	switch (mode) {
-//	case ReadOnly:
-//		inputConsole = GetStdHandle(STD_INPUT_HANDLE);
-//		// set to non-echo and not waiting for ENTER key
-//		SetConsoleMode(inputConsole, ENABLE_PROCESSED_OUTPUT);
-//
-//		inputBuffer.flush();
-//		startReadingThread();
-//		break;
-//	case WriteOnly:
-//		outputConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-//		break;
-//	case ReadWrite:
-//		inputConsole = GetStdHandle(STD_INPUT_HANDLE);
-//		// set to non-echo and not waiting for ENTER key
-//		SetConsoleMode(inputConsole, ENABLE_PROCESSED_OUTPUT);
-//
-//		inputBuffer.flush();
-//		startReadingThread();
-//		outputConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-//		break;
-//	default:
-//		return false;
-//	}
+    switch (mode) {
+        case ReadOnly:
+            inputBuffer.flush();
+            startReadingThread();
+            this->mode = OpenMode::ReadOnly;
+            break;
+        case WriteOnly:
+            this->mode = OpenMode::WriteOnly;
+            break;
+        case ReadWrite:
+            inputBuffer.flush();
+            startReadingThread();
+            this->mode = OpenMode::ReadWrite;
+            break;
+        default:
+            return false;
+    }
 
-//		if(nullptr != outputConsole) {
-//		  // redirect unbuffered STDOUT to the console
-//		  int hConHandle = _open_osfhandle((intptr_t)outputConsole, _O_TEXT);
-//		  FILE *fp = _fdopen(hConHandle, "w");
-//		  *stdout = *fp;
-//		  setvbuf(stdout, NULL, _IONBF, 0);
-//
-//		  // redirect unbuffered STDERR to the console
-//		  hConHandle = _open_osfhandle((intptr_t)outputConsole, _O_TEXT);
-//		  fp = _fdopen( hConHandle, "w" );
-//		  *stderr = *fp;
-//		  setvbuf(stderr, NULL, _IONBF, 0);
-//
-//		  // make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog point to console as well
-//		  std::ios::sync_with_stdio();
-//		}
-//		if(nullptr != inputConsole) {
-//		  // redirect unbuffered STDIN to the console
-//		  int hConHandle = _open_osfhandle((intptr_t)inputConsole, _O_TEXT);
-//		  FILE *fp = _fdopen( hConHandle, "r" );
-//		  *stdin = *fp;
-//		  setvbuf(stdin, NULL, _IONBF, 0);
-//		  // make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog point to console as well
-//		  std::ios::sync_with_stdio();
-//		}
-	return true;
+    //		if(nullptr != outputConsole) {
+    //		  // redirect unbuffered STDOUT to the console
+    //		  int hConHandle = _open_osfhandle((intptr_t)outputConsole, _O_TEXT);
+    //		  FILE *fp = _fdopen(hConHandle, "w");
+    //		  *stdout = *fp;
+    //		  setvbuf(stdout, NULL, _IONBF, 0);
+    //
+    //		  // redirect unbuffered STDERR to the console
+    //		  hConHandle = _open_osfhandle((intptr_t)outputConsole, _O_TEXT);
+    //		  fp = _fdopen( hConHandle, "w" );
+    //		  *stderr = *fp;
+    //		  setvbuf(stderr, NULL, _IONBF, 0);
+    //
+    //		  // make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog point to console as well
+    //		  std::ios::sync_with_stdio();
+    //		}
+    //		if(nullptr != inputConsole) {
+    //		  // redirect unbuffered STDIN to the console
+    //		  int hConHandle = _open_osfhandle((intptr_t)inputConsole, _O_TEXT);
+    //		  FILE *fp = _fdopen( hConHandle, "r" );
+    //		  *stdin = *fp;
+    //		  setvbuf(stdin, NULL, _IONBF, 0);
+    //		  // make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog point to console as well
+    //		  std::ios::sync_with_stdio();
+    //		}
+    return true;
 }
 
-void console_IODevice::close()  noexcept {
-	inputBuffer.flush();
-	stopReadingThread();
-	//inputConsole = nullptr;
-	//outputConsole = nullptr;
-//	FreeConsole();
+void console_IODevice::close() noexcept {
+    inputBuffer.flush();
+    stopReadingThread();
+    mode = OpenMode::NotOpen;
 }
 
 bool console_IODevice::isOpen() const noexcept {
-//	if (nullptr != inputConsole || nullptr != outputConsole) {
-//		return true;
-//	}
-	return false;
+    return mode != OpenMode::NotOpen;
 }
 
 size_t console_IODevice::read(char *buffer, size_t length) noexcept {
-	size_t readDataLength = inputBuffer.getLength();
-	if (readDataLength > length) {
-		readDataLength = length;
-	}
+    if (mode == OpenMode::ReadWrite || mode == OpenMode::ReadOnly) {
+        std::lock_guard<std::mutex> g(inputBufferMutex);
 
-	size_t i;
-	for (i = 0; i < readDataLength; ++i) {
-		if (inputBuffer.isEmpty()) {
-			break;
-		}
-		buffer[i] = inputBuffer.get();
-	}
+        size_t readDataLength = inputBuffer.getLength();
+        if (readDataLength > length) {
+            readDataLength = length;
+        }
 
-	return i;
+        size_t i;
+        for (i = 0; i < readDataLength; ++i) {
+            if (inputBuffer.isEmpty()) {
+                break;
+            }
+            buffer[i] = inputBuffer.get();
+        }
+
+        return i;
+    }
+    return 0;
 }
 
-//size_t console_IODevice::readLine(char *buffer, size_t maxLength) {
+// size_t console_IODevice::readLine(char *buffer, size_t maxLength) {
 //	size_t count = 0;
 //	char single;
 //
@@ -137,73 +129,68 @@ size_t console_IODevice::read(char *buffer, size_t length) noexcept {
 //}
 
 size_t console_IODevice::write(const char *data, size_t length) noexcept {
-
-	size_t bytesWritten = 0;
-	//if (nullptr != outputConsole) {
-		//char *sign = (char*) data;
-		bytesWritten = length;
-		while (length--) {
-			putchar(*data++);
-		}
-		fflush(stdout);
-	//}
-	return (size_t) bytesWritten;
+    size_t bytesWritten = 0;
+    if (mode == OpenMode::ReadWrite || mode == OpenMode::WriteOnly) {
+        if (waddnstr(stdscr, data, length) != ERR) {
+            bytesWritten = length;
+            refresh();
+        }
+    }
+    return bytesWritten;
 }
 
 void console_IODevice::startReadingThread(void) {
-	runReadingThread = true;
-	readingThread = std::thread(&console_IODevice::procThread, this);
+    readingThread = std::jthread(&console_IODevice::procThread, this);
 }
 
 void console_IODevice::stopReadingThread(void) {
-	runReadingThread = false;
-	if (readingThread.joinable()) {
-		readingThread.join();
-	}
+    readingThread.request_stop();
 }
 
-void console_IODevice::procThread(void) {
+void console_IODevice::procThread() {
+    auto stoken = readingThread.get_stop_token();
 
-//	HANDLE console;
-//	INPUT_RECORD buf;
-//	DWORD read;
+    raw();                /* Line buffering disabled	*/
+    keypad(stdscr, TRUE); /* We get F1, F2 etc..		*/
+    noecho();             /* Don't echo() while we do getch */
+    timeout(-1);
 
-	// get console handle
-//	console = GetStdHandle(STD_INPUT_HANDLE);
-//	if (console == INVALID_HANDLE_VALUE)
-		// TODO error code...
-
-	// set to non-echo and not waiting for ENTER key
-//	SetConsoleMode(console, ENABLE_PROCESSED_OUTPUT);
-
-	while (runReadingThread) {
-//		ReadConsoleInput(console, &buf, 1, &read);
-//		if (buf.Event.KeyEvent.bKeyDown && buf.EventType == KEY_EVENT) {
-//			switch (buf.Event.KeyEvent.wVirtualKeyCode) {
-//			case VK_LEFT:
-//				inputBuffer.append(0xe0);
-//				inputBuffer.append(0x4b);
-//				break;
-//			case VK_RIGHT:
-//				inputBuffer.append(0xe0);
-//				inputBuffer.append(0x4d);
-//				break;
-//			case VK_UP:
-//				inputBuffer.append(0xe0);
-//				inputBuffer.append(0x48);
-//				break;
-//			case VK_DOWN:
-//				inputBuffer.append(0xe0);
-//				inputBuffer.append(0x50);
-//				break;
-//			default:
-//				if (buf.Event.KeyEvent.uChar.AsciiChar)
-//					inputBuffer.append((uint8_t) buf.Event.KeyEvent.uChar.AsciiChar);
-//				break;
-//			}
-//		}
-		std::this_thread::sleep_for(std::chrono::milliseconds { 10 });
-	}
+    while (!stoken.stop_requested()) {
+        auto c = getch();
+        if (c != ERR) {
+            std::lock_guard<std::mutex> g(inputBufferMutex);
+            switch (c) {
+                case KEY_LEFT:
+                    inputBuffer.append(0xe0);
+                    inputBuffer.append(0x4b);
+                    break;
+                case KEY_RIGHT:
+                    inputBuffer.append(0xe0);
+                    inputBuffer.append(0x4d);
+                    break;
+                case KEY_UP:
+                    inputBuffer.append(0xe0);
+                    inputBuffer.append(0x48);
+                    break;
+                case KEY_DOWN:
+                    inputBuffer.append(0xe0);
+                    inputBuffer.append(0x50);
+                    break;
+                case KEY_ENTER:
+                    inputBuffer.append('\n');
+                    break;
+                case KEY_BACKSPACE:
+                    inputBuffer.append('\b');
+                    break;
+                case 3:
+                    std::terminate();
+                    break;
+                default:
+                    inputBuffer.append(c);
+                    break;
+            }
+        }
+    }
 }
 
 }  // namespace linux
