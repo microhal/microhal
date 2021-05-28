8fefa377ae4fe6d501b5cc162512f15b4ec213c4
sakoPO <sako.po@gmail.com>
2021-05-28 18:52:35 +0200

    VirtualIODevice moved impementation from .h to .cpp.

--- microhal/core/IODevice/virtualIODevice/virtualIODevice.h
+++ microhal/core/IODevice/virtualIODevice/virtualIODevice.h
@@ -1,8 +1,27 @@
-/*
- * virtualIODevice.h
+/**
+ * @license    BSD 3-Clause
+ * @version    $Id$
+ * @brief
  *
- *  Created on: Nov 19, 2020
- *      Author: pokas
+ * @authors    Pawel Okas
+ *
+ * @copyright Copyright (c) 2020-2021, Pawel Okas
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
+ *        software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef _MICROHAL_VIRTUALIODEVICE_H_
@@ -18,59 +37,28 @@ class VirtualIODevice : public IODevice {
     VirtualIODevice();
     virtual ~VirtualIODevice();
 
-    bool connect(VirtualIODevice &receiver) {
-        if (device == nullptr) {
-            device = &receiver;
-            return true;
-        }
-        return false;
-    }
-    bool disconnect(VirtualIODevice &receiver) {
-        if (device == &receiver) {
-            device = nullptr;
-            return true;
-        }
-        return false;
-    }
+    VirtualIODevice(const VirtualIODevice &) = delete;
+    void operator=(const VirtualIODevice &) = delete;
 
-    bool open(OpenMode mode) noexcept final {
-        if (mode == OpenMode::NotOpen) {
-            this->mode = mode;
-            return true;
-        }
-        if (!isOpen()) {
-            if (device || (mode == OpenMode::ReadOnly)) {
-                this->mode = mode;
-                return true;
-            }
-        }
-        return false;
-    }
+    bool connect(VirtualIODevice &receiver);
+    bool disconnect(VirtualIODevice &receiver);
+
+    bool open(OpenMode mode) noexcept final;
 
     void close() noexcept final { mode = OpenMode::NotOpen; }
 
     bool isOpen() const noexcept final { return mode != OpenMode::NotOpen; }
 
-    size_t read(char *buffer, size_t length) noexcept final {
-        if (mode == OpenMode::ReadOnly || mode == OpenMode::ReadWrite) {
-            return receiveBuffer.read(buffer, length);
-        }
-        return 0;
-    }
+    size_t read(char *buffer, size_t length) noexcept final;
 
     size_t availableBytes() const noexcept final { return receiveBuffer.getLength(); }
 
-    size_t write(const char *data, size_t length) noexcept final {
-        if (mode == OpenMode::WriteOnly || mode == OpenMode::ReadWrite) {
-            return device->receiveBuffer.write(data, length);
-        }
-        return 0;
-    }
+    size_t write(const char *data, size_t length) noexcept final;
 
  private:
     OpenMode mode = OpenMode::NotOpen;
     VirtualIODevice *device = nullptr;
-    CyclicBuffer_data<char, 100> receiveBuffer;
+    CyclicBuffer_data<char, 100> receiveBuffer{};
 };
 
 } /* namespace microhal */
