3689f01c74139ce3f7be760872a0f2cb1d619e28
sakoPO <sako.po@gmail.com>
2021-06-02 12:35:23 +0200

    Port STM32F1xx, updated clock manager.

--- microhal/ports/stm32f1xx/clockManager.h
+++ microhal/ports/stm32f1xx/clockManager.h
@@ -8,10 +8,14 @@
 #ifndef SRC_MICROHAL_PORTS_STM32F1XX_CLOCKMANAGER_H_
 #define SRC_MICROHAL_PORTS_STM32F1XX_CLOCKMANAGER_H_
 
+#include "mcuCapabilities.h"
+
 #include "../stmCommon/clockManager/adcClock.h"
 #include "../stmCommon/clockManager/afioClock.h"
 #include "../stmCommon/clockManager/backupRegistersClock.h"
+#if _MICROHAL_STM32F1XX_HAS_ETH == 1
 #include "../stmCommon/clockManager/ethernetClock.h"
+#endif
 #include "../stmCommon/clockManager/hse.h"
 #include "../stmCommon/clockManager/hsi.h"
 #include "../stmCommon/clockManager/lse.h"
