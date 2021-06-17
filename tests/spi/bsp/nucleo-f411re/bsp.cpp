66872c74543d19b6e72b31677397908fe75a80ab
sakoPO <sako.po@gmail.com>
2021-06-17 23:45:10 +0200

    Test, added SPI Test project files.

---
+++ tests/spi/bsp/nucleo-f411re/bsp.cpp
@@ -0,0 +1,90 @@
+/**
+ * @file
+ * @license    BSD 3-Clause
+ * @version    $Id$
+ * @brief      board support package for nucleo-f411re board
+ *
+ * @authors    Pawel Okas
+ *
+ * @copyright Copyright (c) 2021, Pawel Okas
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 	   2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * 	      documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
+ *        software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "bsp.h"
+#include "microhal.h"
+#include "ports/stm32f3xx/timer_stm32f3xx.h"
+#include "ports/stm32f4xx/IOManager_stm32f4xx.h"
+#include "ports/stm32f4xx/clockManager.h"
+
+using namespace microhal;
+using namespace microhal::stm32f4xx;
+
+using Timer = stm32f4xx::Timer;
+
+namespace bsp {
+void configureSerialPort(microhal::SerialPort &serial, microhal::SerialPort::BaudRate baudrate) {
+    serial.setBaudRate(baudrate);
+    serial.setDataBits(microhal::SerialPort::Data8);
+    serial.setStopBits(microhal::SerialPort::OneStop);
+    serial.setParity(microhal::SerialPort::NoParity);
+    serial.open(microhal::SerialPort::ReadWrite);
+}
+
+bool init() {
+    IOManager::routeSerial<2, Txd, IOPin{IOPin::PortA, 2}>();
+    IOManager::routeSerial<2, Rxd, IOPin{IOPin::PortA, 3}>();
+
+    configureSerialPort(debugPort, microhal::SerialPort::Baud115200);
+
+    return true;
+}
+
+}  // namespace bsp
+
+extern "C" ssize_t _write_r(struct _reent *r, int file, const void *buf, size_t nbyte) {
+    return bsp::debugPort.write((const char *)buf, nbyte);
+}
+
+Timer timer(registers::tim2);
+
+extern "C" void RTOS_AppConfigureTimerForRuntimeStats(void) {
+    timer.setClockSource(Timer::ClockSource::Internal);
+    timer.setPrescaler(6400);
+    timer.setMode(Timer::Mode::EdgeAligned);
+    timer.setDirection(Timer::Direction::UpCounter);
+    timer.setValue(0);
+    timer.generateEvent(Timer::Event::Update);
+    timer.enable();
+}
+
+extern "C" uint32_t RTOS_AppGetRuntimeCounterValueFromISR(void) {
+    return (uint32_t)registers::tim2->cnt.volatileLoad();
+}
+
+extern "C" int main(int, void *);
+static void run_main(void *) {
+    main(0, nullptr);
+}
+
+void hardwareConfig(void) {
+    TaskHandle_t xHandle = NULL;
+
+    xTaskCreate(run_main, "main", 600, NULL, tskIDLE_PRIORITY, &xHandle);
+
+    vTaskStartScheduler();
+}
