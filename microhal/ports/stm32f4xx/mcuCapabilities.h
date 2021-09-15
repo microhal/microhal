457dfbffe1ae7d570f96ac225fa4a44e3fa3d64f
sakoPO <sako.po@gmail.com>
2021-09-15 19:01:26 +0200

    Port STM32F4xx, added spiMaxCLKFrequency into mcuCapabilities.h

--- microhal/ports/stm32f4xx/mcuCapabilities.h
+++ microhal/ports/stm32f4xx/mcuCapabilities.h
@@ -8,6 +8,10 @@
 #ifndef SRC_THIRD_PARTY_MICROHAL_PORTS_STM32F4XX_MCUCAPABILITIES_H_
 #define SRC_THIRD_PARTY_MICROHAL_PORTS_STM32F4XX_MCUCAPABILITIES_H_
 
+namespace microhal {
+namespace stm32f4xx {
+namespace hardwareLimits {
+
 #define _MICROHAL_STM32F4XX_HAS_SPI1 1
 #define _MICROHAL_STM32F4XX_HAS_SPI2 1
 #define _MICROHAL_STM32F4XX_HAS_SPI3 1
@@ -19,5 +23,11 @@
 #define _MICROHAL_STM32F4XX_HAS_USART3 1
 #define _MICROHAL_STM32F4XX_HAS_USART4 1
 #define _MICROHAL_STM32F4XX_HAS_USART6 1
+
+constexpr const uint32_t spiMaxCLKFrequency = 32'000'000;  // in [Hz]
+
+}  // namespace hardwareLimits
+}  // namespace stm32f4xx
+}  // namespace microhal
 
 #endif /* SRC_THIRD_PARTY_MICROHAL_PORTS_STM32F4XX_MCUCAPABILITIES_H_ */
