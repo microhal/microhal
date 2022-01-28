c3503bd4cdcd10e3875116ce692a91fd77809175
sakoPO <sako.po@gmail.com>
2022-01-28 17:34:24 +0100

    Port STM32G0xx, Interrupt controller updated functions declaratons.

--- microhal/ports/stm32g0xx/interruptController/interruptController_stm32g0x1.cpp
+++ microhal/ports/stm32g0xx/interruptController/interruptController_stm32g0x1.cpp
@@ -246,7 +246,7 @@ void disableSPIInterrupt(uint_fast8_t spiNumber) {
     }
 }
 
-void enableUSARTInterrupt(uint8_t usartNumber, uint32_t priority) {
+void enableUSARTInterrupt(uint_fast8_t usartNumber, uint32_t priority) {
     assert(usartNumber > 0);
     assert(usartNumber <= 6);
 
@@ -264,7 +264,7 @@ void enableUSARTInterrupt(uint8_t usartNumber, uint32_t priority) {
     }
 }
 
-void setUSARTInterruptPriority(uint8_t usartNumber, uint32_t priority) {
+void setUSARTInterruptPriority(uint_fast8_t usartNumber, uint32_t priority) {
     assert(usartNumber > 0);
     assert(usartNumber <= 6);
 
@@ -277,7 +277,7 @@ void setUSARTInterruptPriority(uint8_t usartNumber, uint32_t priority) {
     }
 }
 
-void disableUSARTInterrupt(uint8_t usartNumber) {
+void disableUSARTInterrupt(uint_fast8_t usartNumber) {
     assert(usartNumber > 0);
     assert(usartNumber <= 6);
 
